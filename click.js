631a2aef01eddb65b0de50907bb36d21ec72b7e2
elim2513 <8elim@stpatrick.org>
2017-03-22 11:27:59 -0500

    Add files via upload
    Update 2.0

---
+++ click.js
@@ -0,0 +1,17 @@
+//declare samecolor() here
+function samecolor(){
+  let color = "#A4FFE7"
+  const doc = document;
+
+  let divs = doc.querySelectorAll("#Samecolor div");
+  for(){
+    divs[i].style.backgroundColor =
+  }
+
+
+
+}
+//declare diffcolor() here
+function diffcolor(){
+
+}
